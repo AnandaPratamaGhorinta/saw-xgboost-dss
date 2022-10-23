4c1d821a5a2e34e8c2f8e6a1003fe065ad28aef1
AnandaPratamaGhorinta <ghorinta@gmail.com>
2022-10-23 15:11:13 +0700

    main : test prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import TableView from './uiComponent/TableView';
-import { tableViewDataTest } from './testingData/tableViewDataTest';
+import React from "react";
+import "./App.css";
+import TableView from "./uiComponent/TableView";
+import { tableViewDataTest } from "./testingData/tableViewDataTest";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <TableView data={tableViewDataTest}/>
-      </header>
-    </div>
+    <>
+      <TableView data={tableViewDataTest} />
+    </>
   );
 }
 
--- src/testingData/tableViewDataTest.ts
+++ src/testingData/tableViewDataTest.ts
@@ -1,8 +1,55 @@
 export const tableViewDataTest =[{
-
     name:'ananda',
     age : '12'
 },{
     name:'lont',
     age :'13'
-}]
+},{
+    name:'lont',
+    age :'13'
+},{
+    name:'lont',
+    age :'13'
+},{
+    name:'lont',
+    age :'13'
+},{
+    name:'lont',
+    age :'13'
+},{
+    name:'lont',
+    age :'13'
+},{
+    name:'lont',
+    age :'13'
+},{
+    name:'lont',
+    age :'13'
+},{
+    name:'lont',
+    age :'13'
+},{
+    name:'lont',
+    age :'13'
+},{
+    name:'lont',
+    age :'13'
+},{
+    name:'lont',
+    age :'13'
+},{
+    name:'lont',
+    age :'13'
+},{
+    name:'lont',
+    age :'13'
+},{
+    name:'lont',
+    age :'13'
+},{
+    name:'lont',
+    age :'13'
+},{
+    name:'lont',
+    age :'13'
+},]
