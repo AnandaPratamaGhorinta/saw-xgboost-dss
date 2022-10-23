940d5c53cd3e304a39eb667053915f0b35c34072
AnandaPratamaGhorinta <ghorinta@gmail.com>
2022-10-23 16:02:30 +0700

    main : service setup

---
+++ src/service/endpoints/endpoints.ts
@@ -0,0 +1,3 @@
+export const endpoints =  {
+    tableData : 'http://localhost:3005/users'
+}
---
+++ src/service/getUserService.ts
@@ -0,0 +1,11 @@
+import { Users } from "../uiComponent/type";
+import axios from "axios";
+import { endpoints } from "./endpoints/endpoints";
+
+export default function GetUserService () {
+
+    return axios({
+        url:''
+    })
+}
+   
