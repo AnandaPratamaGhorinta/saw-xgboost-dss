f4ddee79a3029924150df8913da186379dc2b84b
AnandaPratamaGhorinta <ghorinta@gmail.com>
2023-04-02 20:47:05 +0700

    TEST-PUSH

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,7 @@
 import "./App.css";
-import TableView from "./uiComponent/TableView";
-import {
-  tableViewDataTest,
-  tableViewDataTest2,
-} from "./testingData/tableViewDataTest";
-import Tabs, { TabPanesProps } from "./uiComponent/Tabs";
 
 function App() {
-  const tabPanes: TabPanesProps[] = [
-    {
-      children: <TableView data={tableViewDataTest2} />,
-      key: "1",
-      tab: "Test tab 1",
-    },
-    {
-      children: <TableView data={tableViewDataTest} />,
-      key: "2",
-      tab: "Test tab 2",
-    },
-  ];
-  return (
-    <>
-      <Tabs tabPanes={tabPanes} />
-    </>
-  );
+  return <>Testing</>;
 }
 
 export default App;
