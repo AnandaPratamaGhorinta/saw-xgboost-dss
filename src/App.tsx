8080297f8e069c124deb2faeb9a6152b847ea926
fernando6500 <fernandokho6500@gmail.com>
2023-04-03 19:24:18 +0700

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,62 @@
+import React, { useState } from "react";
+import { Login } from "./login/login";
+
 interface User {
   name: string;
   age?: string;
 }
 
 function App() {
-  const users: User[] = [];
+  const [users, setUsers] = useState<User[]>([]);
+  let tableUserData: User[] = [];
+  const tableUserDataRenderer: React.ReactNode[] = [];
+
+  function handleAddUser() {
+    const data: User = {
+      name: (document.getElementById("name") as HTMLInputElement)?.value,
+      age: (document.getElementById("age") as HTMLInputElement)?.value,
+    };
+    setUsers(tableUserData);
+  }
+
+  function handleDeleteUser() {
+    tableUserData.pop();
+    setUsers(tableUserData);
+  }
+  console.log(users);
+  users.forEach((user) => {
+    const data = (
+      <tr>
+        <td>{user.name}</td>
+        <td>{user.age}</td>
+      </tr>
+    );
+    tableUserDataRenderer.push(data);
+  });
 
   return (
     <>
       <table border={100}>
         <tr>
-          <th>Company</th>
-          <th>Contact</th>
-          <th>Country</th>
+          <th>Name</th>
+          <th>Age</th>
         </tr>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
-        </tr>
-        <tr>
-          <td>Centro comercial Moctezuma</td>
-          <td>Francisco Chang</td>
-          <td>Mexico</td>
-        </tr>
+        {tableUserDataRenderer}
       </table>
-
-      <form>
-        <p>
-          name : <input type="text" id="name" />
-        </p>
-        <p>
-          age :<input type="text" id="age" />
-        </p>
-      </form>
+      <p>
+        name : <input type="text" id="name" />
+      </p>
+      <p>
+        age :<input type="text" id="age" />
+      </p>
+      <p>
+        <button id="delete" onClick={handleDeleteUser}>
+          DELETE
+        </button>
+        <button id="add" onClick={handleAddUser}>
+          ADD
+        </button>
+      </p>
       <p>
         To understand the example better, we have added borders to the table.
       </p>
